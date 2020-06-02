21da033a93189bcbcedb691773840acc8b9f8cb9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-02 12:37:14 +0900

    Add codes

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ license: The MIT License, Copyright (c) 2020 YUKI "Piro" Hiroshi
+*/
+
+import * as Assert from './src/assert.js';
+import * as Runner from './src/runner.js';
+
+export const assert = Assert;
+export const run = Runner.run;
