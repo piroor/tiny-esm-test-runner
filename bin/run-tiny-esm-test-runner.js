cb08d7832b9eb2d8ef11ff9b527e1fb151d6ac66
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-02 19:01:03 +0900

    Don't call exit() by runner itself

--- bin/run-tiny-esm-test-runner.js
+++ bin/run-tiny-esm-test-runner.js
@@ -7,4 +7,5 @@ import { resolve } from 'path';
 import { run } from '../index.js';
 
 Promise.all(process.argv.slice(2).map(path => import(resolve(path))))
-  .then(testCaseModules => run(testCaseModules));
+  .then(testCaseModules => run(testCaseModules))
+  .then(result => process.exit(result.failureCount + result.errorCount > 0 ? 1 : 0));
