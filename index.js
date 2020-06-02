da6e9f8ade6b2f7d7be95c8d39f4f110b8c1cb1e
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-02 16:09:35 +0900

    Rename directory

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  license: The MIT License, Copyright (c) 2020 YUKI "Piro" Hiroshi
 */
 
-import * as Assert from './src/assert.js';
-import * as Runner from './src/runner.js';
+import * as Assert from './lib/assert.js';
+import * as Runner from './lib/runner.js';
 
 export const assert = Assert;
 export const run = Runner.run;
