29b786174765e7c45e2c6aab1c06c39354d6abd2
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-04 14:53:36 +0900

    Allow to change stdout

--- bin/run-tiny-esm-test-runner.js
+++ bin/run-tiny-esm-test-runner.js
@@ -7,5 +7,7 @@ import { resolve } from 'path';
 import { run } from '../index.js';
 
 Promise.all(process.argv.slice(2).map(path => import(resolve(path))))
-  .then(testCaseModules => run(testCaseModules))
+  .then(testCaseModules => run(testCaseModules, {
+    stdout: (...args) => process.stdout.write(...args)
+  }))
   .then(result => process.exit(result.failure + result.error > 0 ? 1 : 0));
