7e0288d7ac26d17107615b20459126a16a9dedff
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-09-05 14:31:14 +0900

    Better support of the Windows platform

--- bin/run-tiny-esm-test-runner.js
+++ bin/run-tiny-esm-test-runner.js
@@ -4,8 +4,12 @@
 */
 
 import { resolve } from 'path';
+import { default as glob } from 'glob';
+import { default as fileUrl } from 'file-url';
 import { run } from '../index.js';
 
-Promise.all(process.argv.slice(2).map(path => import(resolve(path))))
+const files = process.argv.slice(2).map(path => glob.sync(path)).flat();
+const urls = files.map(path => fileUrl(path));
+Promise.all(urls.map(url => import(url)))
   .then(testCaseModules => run(testCaseModules))
   .then(result => process.exit(result.failure + result.error > 0 ? 1 : 0));
